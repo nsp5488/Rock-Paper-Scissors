7f781d9ac6323722cb3b253d3d8edfcab24ae702
nsp5488 <nsp5488@g.rit.edu>
2025-01-30 16:14:53 -0500

    Added a basic UI

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-console.log("hello, world");
-
 function getComputerChoice() {
   roll = Math.random();
   if (roll < 0.33) {
@@ -19,8 +17,7 @@ function getHumanChoice() {
   }
   return choice;
 }
-
-function playRound(humanChoice, compChoice) {
+function humanWon(humanChoice, compChoice) {
   if (humanChoice == "rock" && compChoice == "scissors") {
     return true;
   } else if (humanChoice == "paper" && compChoice == "rock") {
@@ -28,34 +25,81 @@ function playRound(humanChoice, compChoice) {
   } else if (humanChoice == "scissors" && compChoice == "paper") {
     return true;
   }
+
   return false;
 }
-function playGame() {
-  let humanScore = 0;
-  let compScore = 0;
-  for (let i = 0; i < 5; i++) {
-    choice = getHumanChoice();
-    compChoice = getComputerChoice();
-
-    if (choice == compChoice) {
-      console.log("It's a draw!");
-      continue;
-    }
-
-    if (playRound(choice, compChoice)) {
-      console.log(`You win! ${choice} beats ${compChoice}`);
-      humanScore++;
-    } else {
-      console.log(`You lost! ${compChoice} beats ${choice}`);
-      compScore++;
-    }
+function playRound(humanChoice, compChoice, scores) {
+  const roundResult = document.querySelector("p");
+  if (humanChoice == compChoice) {
+    roundResult.textContent = "It's a draw!";
+    return scores;
   }
-  if (humanScore == compScore) {
-    console.log(`Its a tie! both players have ${humanScore}`);
+
+  if (humanWon(humanChoice, compChoice)) {
+    roundResult.textContent = `You win! ${humanChoice} beats ${compChoice}`;
+    scores[0]++;
+  } else {
+    roundResult.textContent = `You lost! ${compChoice} beats ${humanChoice}`;
+    scores[1]++;
+  }
+
+  return scores;
+}
+
+function updateScores(scores) {
+  if (scores[0] == 5 || scores[1] == 5) {
+    const results = document.querySelector("h1");
+    let wonOrLost = scores[0] > scores[1] ? "won" : "lost";
+    results.textContent = `You ${wonOrLost}!\nFinal scores: You: ${scores[0]}, Computer: ${scores[1]}`;
+    document.querySelectorAll("button").forEach((btn) => {
+      btn.disabled = true;
+    });
     return;
   }
-  console.log(
-    `You ${humanScore > compScore ? "won" : "lost"}!\nFinal scores: You: ${humanScore}, Computer: ${compScore}`,
-  );
+
+  document.querySelector("h1").textContent =
+    `Your score: ${scores[0]} Computer score: ${scores[1]}`;
+}
+function playGame() {
+  buildUI();
+
+  const btns = document.querySelectorAll("button");
+
+  let humanScore = 0;
+  let compScore = 0;
+  btns[0].onclick = () => {
+    scores = playRound("rock", getComputerChoice(), [humanScore, compScore]);
+    humanScore = scores[0];
+    compScore = scores[1];
+    updateScores(scores);
+  };
+  btns[1].onclick = () => {
+    playRound("paper", getComputerChoice(), humanScore, compScore);
+  };
+  btns[2].onclick = () => {
+    playRound("scissors", getComputerChoice(), humanScore, compScore);
+  };
+}
+function buildUI() {
+  const rockBtn = document.createElement("button");
+  const paperBtn = document.createElement("button");
+  const scissorsBtn = document.createElement("button");
+
+  rockBtn.textContent = "Rock";
+  paperBtn.textContent = "Paper";
+  scissorsBtn.textContent = "Scissors";
+
+  const body = document.querySelector("body");
+
+  body.appendChild(rockBtn);
+  body.appendChild(paperBtn);
+  body.appendChild(scissorsBtn);
+  const results = document.querySelector("div");
+  const resultsLine = document.createElement("h1");
+  const roundResult = document.createElement("p");
+  roundResult.textContent = "";
+  resultsLine.textContent = `Your score: 0 Computer score: 0`;
+  results.appendChild(resultsLine);
+  results.appendChild(roundResult);
 }
 playGame();
